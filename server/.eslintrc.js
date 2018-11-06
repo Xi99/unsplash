be50fc4544ac12d5fdbce075a84999f65531a3c2
Sean Lorman <sean.lorman@gmail.com>
2018-11-05 18:35:09 -0700

    setup of the express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    rules: {
+        'no-console': 'off',
+    },
+    "parserOptions": {
+        "ecmaVersion": 2017
+    },
+
+    "env": {
+        "es6": true
+    }
+};
+
+  
